bf03c65a8ee13405227e482b84bea861da980766
Yangfan Zhang <yf_chang@icloud.com>
2020-06-09 19:32:29 +1000

    Added icon

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ app.get("/", function(req, res) {
         const weatherData =JSON.parse(data);
         const temp = weatherData.main.temp;
         const weatherDescription = weatherData.weather[0].description;
-        res.send("<h1>The temperature in Brisbane is" + temp + "</h1>");
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        res.write("<p>The weather is currently" + weatherDescription + "</p>")
+        res.write("<h1>The temperature in Brisbane is" + temp + "</h1>");
+        res.write("<img src=" + imageURL + ">");
+        res.send();
     })
     })
 })
