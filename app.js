c7380f10e59dd6cbb714e5c1a0a8614c2ce9c880
Yangfan Zhang <yf_chang@icloud.com>
2020-06-09 20:11:29 +1000

    Using body parser to parse post requests to the server

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const https = require("https");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Brisbane&appid=cb4ad0a186e8912d5bbbf8bcfd299e1a";
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const apiKey = "cb4ad0a186e8912d5bbbf8bcfd299e1a";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query + "&appid=" + apiKey + "&units=" + unit;
     https.get(url, function(response){
     console.log(response.statusCode);
 
@@ -14,12 +23,18 @@ app.get("/", function(req, res) {
         const icon = weatherData.weather[0].icon;
         const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
         res.write("<p>The weather is currently" + weatherDescription + "</p>")
-        res.write("<h1>The temperature in Brisbane is" + temp + "</h1>");
+        res.write("<h1>The temperature in " + query +  "is " + temp + "</h1>");
         res.write("<img src=" + imageURL + ">");
         res.send();
     })
     })
 })
+
+
+
+
+
+
 
 app.listen(3000, function() {
     console.log("3000");
